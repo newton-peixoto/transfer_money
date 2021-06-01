b79e21813fd788c4447fffa9bc5b88a0672c046c
Newton <newton@pop-os.localdomain>
2021-06-01 20:49:04 -0300

    UserServiceTest

---
+++ src/tests/UserServiceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Services\UserService;
+
+class UserServiceTest extends TestCase
+{
+
+    public function setUp()  : void {
+        parent::setUp();
+        $this->artisan('migrate:fresh');
+    }
+
+    public function testGivenValidDataShouldCreateUser(){
+        $data = ['name' => 'tester', 
+                'email' => 'tester@email.com', 
+                'password' => '123123',
+                'identifier' => '26791226072'];
+        $userService = new UserService;
+        $user = $userService->createUser($data);
+
+        $this->assertEquals($user->email, $data['email']);
+    }
+
+    public function testGivenInValidDataShouldNotCreateUser(){
+        $this->expectExceptionMessage('Error while creating user in the database!');
+        $data = ['name' => 'tester', 
+                'email' => 'tester@email.com', 
+                'identifier' => '26791226072'];
+        $userService = new UserService;
+        $user = $userService->createUser($data);
+    }
+ 
+}
